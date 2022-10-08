15f52c1760efb4aaee6bf186e555c577bdc337bd
=kaulfield <kaulfield23@gmail.com>
2022-10-08 21:10:40 +0200

    added textarea

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -8,5 +8,5 @@ export default async function handlePosts(
   const createTableQuery =
     "CREATE TABLE IF NOT EXISTS posts(id serial PRIMARY KEY, userId TEXT, content TEXT, title TEXT, date DATE DEFAULT CURRENT_DATE, UNIQUE(userId), CONSTRAINT fk_userId FOREIGN KEY(userId) REFERENCES userAccounts(userId));";
   await execQuery(createTableQuery);
-  console.log("hey");
+  return res.status(200).end();
 }
--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,11 +1,11 @@
-import { Avatar, Typography } from "@mui/material";
+import { Avatar, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { NextPage } from "next";
 import React, { useContext, useEffect, useState } from "react";
 import { LoggedInContext } from "../src/components/context/LoggedInContext";
-
+import TextareaAutosize from "@mui/base/TextareaAutosize";
 const Posts: NextPage = () => {
-  const { userId, userAvatarColor } = useContext(LoggedInContext);
+  const { userId, isLoggedIn } = useContext(LoggedInContext);
   const [userInformation, setUserInformation] = useState({
     firstName: "",
     lastName: "",
@@ -13,17 +13,6 @@ const Posts: NextPage = () => {
 
   useEffect(() => {
     const getInfo = async () => {
-      //   if (userId) {
-      //     const res = await fetch(`/api/userInfo?userId=${userId}`, {
-      //       method: "GET",
-      //       headers: { "Content-Type": "application/json" },
-      //     });
-      //     const info = await res.json();
-      //     setUserInformation({
-      //       firstName: info.firstName,
-      //       lastName: info.lastName,
-      //     });
-      //   }
       const res = await fetch(`/api/posts`, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
@@ -33,7 +22,57 @@ const Posts: NextPage = () => {
     getInfo();
   }, [userId]);
 
-  return <></>;
+  return (
+    <>
+      {isLoggedIn && (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            margin: { xs: "80px auto", sm: "200px auto" },
+            flexDirection: "column",
+            maxWidth: "800px",
+            fontFamily: "monospace",
+            backgroundColor: "beige",
+            padding: "20px",
+          }}
+        >
+          <Typography sx={{ fontFamily: "monospace", fontSize: "1.5rem" }}>
+            Title
+          </Typography>
+          <TextField
+            id="outlined-basic"
+            label="Title"
+            variant="outlined"
+            sx={{ backgroundColor: "white" }}
+          />
+          <Typography sx={{ fontFamily: "monospace", fontSize: "1.5rem" }}>
+            Content
+          </Typography>
+          <TextareaAutosize
+            aria-label="minimum height"
+            minRows={10}
+            placeholder="Content"
+          />
+        </Box>
+      )}
+      {!isLoggedIn && (
+        <Box
+          sx={{
+            backgroundColor: "#fef2a1",
+            width: { xs: "300px", md: "500px" },
+            margin: "100px auto",
+            p: 5,
+            textAlign: "center",
+          }}
+        >
+          <Typography sx={{ fontFamily: "monospace", fontSize: "1.5rem" }}>
+            For using this board, You need to log in!
+          </Typography>
+        </Box>
+      )}
+    </>
+  );
 };
 
 export default Posts;
